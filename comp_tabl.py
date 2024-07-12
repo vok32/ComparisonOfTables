43287420c40eb6a10393436ce95892d2d533439c
Roman Matyushenko <roman.matyushenko.03@mail.ru>
2024-07-13 01:30:48 +0300

    v2.0

--- comp_tabl.py
+++ comp_tabl.py
@@ -107,14 +107,19 @@ def compare_excel_tables(file1_path, file2_path, output_path, save_option, key_c
     unused_columns1 = columns1 - columns2
     unused_columns2 = columns2 - columns1
 
+    # Определяем фактическое количество строк в новом файле
+    max_rows = new_sheet.max_row  # Получаем количество строк в new_sheet
+
     for col_name in unused_columns1:
+        if col_name in table2.columns:
+            continue
         col_index = table2.columns.get_loc(col_name) + 1
-        for row in range(1, len(table2) + 2):  # Включаем заголовок
+        for row in range(1, max_rows + 1):  # Включаем заголовок
             new_sheet.cell(row=row, column=col_index).fill = fill_light_orange
 
     for col_name in unused_columns2:
         col_index = table2.columns.get_loc(col_name) + 1
-        for row in range(1, len(table2) + 2):  # Включаем заголовок
+        for row in range(1, max_rows + 1):  # Включаем заголовок
             new_sheet.cell(row=row, column=col_index).fill = fill_light_orange
 
     # Генерация уникального имени файла для сохранения
@@ -209,7 +214,7 @@ def select_files(root):
 
         window = Toplevel(root)
         window.title("Выберите столбец для сравнения")
-        window.geometry(f"400x200+{position[0]}+{position[1]}")
+        window.geometry(f"400x150+{position[0]}+{position[1]}")
 
         label = Label(window, text=f"Выберите столбец для сравнения из {compare_file}:")
         label.pack(pady=10)
